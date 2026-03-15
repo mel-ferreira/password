663c478902a3b81be90fdb8731e0d77d3b41aa1c
mel-ferreira <melissamouraf@gmail.com>
2026-03-15 15:40:48 -0300

    [FEATURE] Implementação: Logs para saber quantas validações rodaram

--- src/main/java/api/password/service/ValidadorSenhaService.java
+++ src/main/java/api/password/service/ValidadorSenhaService.java
@@ -1,10 +1,14 @@
 package api.password.service;
 import api.password.service.validator.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class ValidadorSenhaService {
+
+    private static final Logger log = LoggerFactory.getLogger(ValidadorSenhaService.class);
 
     private final List<SenhaValidacao> validacoes;
 
@@ -13,6 +17,8 @@ public class ValidadorSenhaService {
     }
 
     public boolean validador(String senha) {
+
+        log.debug("Rodando {} validações", validacoes.size());
 
         for (SenhaValidacao validacao : validacoes) {
             if (!validacao.validador(senha)) {

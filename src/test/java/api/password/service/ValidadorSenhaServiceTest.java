a8200ae87026a534d39e29775fc8b21a2777baf9
mel-ferreira <melissamouraf@gmail.com>
2026-03-15 15:30:42 -0300

    [FEATURE] Implementação: Melhoria do teste no Service (@DisplayNamed, @Nested,@ParameterizedTest)

--- src/test/java/api/password/service/ValidadorSenhaServiceTest.java
+++ src/test/java/api/password/service/ValidadorSenhaServiceTest.java
@@ -2,44 +2,70 @@ package api.password.service;
 
 import api.password.exception.RegraNegocioException;
 import api.password.service.validator.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Validador de Senha Service")
 class ValidadorSenhaServiceTest {
 
-    private final ValidadorSenhaService service =
-            new ValidadorSenhaService(List.of(
-                    new NaoVaziaValidacao(),
-                    new MinimoCaracteresValidacao(),
-                    new LetraMaiusculaValidacao(),
-                    new LetraMinusculaValidacao(),
-                    new UmDigitoValidacao(),
-                    new EspacoBrancoValidacao(),
-                    new CaractereEspecialValidacao(),
-                    new RepetirCaractereValidacao()
-            ));
+    private ValidadorSenhaService service;
 
-    @Test
-    void deveValidarSenhaCompleta() {
-
-        String senha = "Ab1@cdefg";
-
-        boolean resultado = service.validador(senha);
-
-        assertTrue(resultado);
+    @BeforeEach
+    void setUp() {
+       service = new ValidadorSenhaService(
+                List.of(
+                new NaoVaziaValidacao(),
+                new MinimoCaracteresValidacao(),
+                new LetraMaiusculaValidacao(),
+                new LetraMinusculaValidacao(),
+                new UmDigitoValidacao(),
+                new EspacoBrancoValidacao(),
+                new CaractereEspecialValidacao(),
+                new RepetirCaractereValidacao()
+        ));
     }
+    @Nested
+    @DisplayName("Quando a senha é válida")
+    class SenhaValida
+    {
+        @Test
+        @DisplayName("Deve retornar true")
+        void deveRetornarTrue()
+        {
+            String senha = "AbTp9!fok";
 
-    @Test
-    void deveFalharQuandoSenhaInvalida() {
+            boolean resultado = service.validador(senha);
+            assertTrue(resultado);
+        }
+    }
+    @Nested
+    @DisplayName("Quando a senha é inválida")
+    class SenhaInvalida
+    {
+        @ParameterizedTest(name = "Senha inválida: {0}")
+        @ValueSource(strings = {
+                "",
+                "abcdefghi",
+                "ABCDEFGHI",
+                "Abcdefghi",
+                "Abcdefg1",
+                "AbTp9!foo"
+        })
+        @DisplayName("Deve lançar exceção para senhas inválidas")
+        void deveLancarExcecao(String senha) {
 
-        String senha = "abcdefghi";
-
-        assertThrows(
-                RegraNegocioException.class,
-                () -> service.validador(senha)
-        );
+            assertThrows(
+                    RegraNegocioException.class,
+                    () -> service.validador(senha)
+            );
+        }
     }
 }
